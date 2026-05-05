24d6a8091221c02d09518ee339d5aa2e5e26f854
Manuel <manuelv0703@gmail.com>
2026-05-05 11:10:44 -0500

    added lua script for a transparent background

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        style = "night", -- options: moon, night
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+}
